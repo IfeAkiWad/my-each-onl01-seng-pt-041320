c1337e88c85fbe21a981caaba7ede797629d9198
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-23 01:41:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ def my_each(array)       # put argument(s) here
   # code here
 end
 i = 0
-  while i < array.length do 
+  while i < array.length do
   yield(array[i])
-  i += 1 
-  
+  i += 1
+
   end
 end
